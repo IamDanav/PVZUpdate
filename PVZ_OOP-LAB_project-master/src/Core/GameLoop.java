a89c6fe459533be5e5a9841109015add3aeda055
Dany <dannismeh@gmail.com>
2026-05-25 22:34:14 +0700

    Delete PVZ_OOP-LAB_project-master directory

--- PVZ_OOP-LAB_project-master/src/Core/GameLoop.java
+++
@@ -1,9 +0,0 @@
-package Core;
-
-import javax.swing.Timer;
-
-public class GameLoop {
-    public GameLoop(Runnable update) {
-        new Timer(100, e -> update.run()).start();
-    }
-}
--- PVZ_OOP-LAB_project-master/src/Entities/Zombie/ConeHead.java
+++
@@ -1,8 +0,0 @@
-package Entities.Zombie;
-import Entities.Zombies;
-public class ConeHead extends Zombies {
-
-    public ConeHead(int row) {
-        super(row, 200, 1);
-    }
-}
